1b42c3af123df88bc2580033d8e091d554940918
Gustavo <freazesss@gmail.com>
2020-03-30 11:04:03 -0300

    Add files via upload

---
+++ Colour/__init__.py
@@ -0,0 +1,12 @@
+class TextColor:
+	def __init__(self):
+		self.close = "\033[m"
+		self.red = "\033[0;31m"
+		self.green = "\033[0;32m"
+		self.yellow = "\033[0;33m"
+		self.blue = "\033[0;34m"
+		self.purple = "\033[0;35m"
+		self.aqua = "\033[0;36m"
+		self.grey = "\033[0;37m"
+
+text = TextColor()
